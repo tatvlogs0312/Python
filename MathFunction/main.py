d068a34ee871909cf79193b5e2c19ef087bf8982
tatuan0312 <85239207+tatvlogs0312@users.noreply.github.com>
2022-12-18 22:27:28 +0700

    //Math function

---
+++ MathFunction/main.py
@@ -0,0 +1,12 @@
+import math
+
+# todo: Math function
+x = 2.6
+# round(x) => làm tròn x
+print(round(x))
+
+# math.ceil(x) => làm tròn lên
+print(math.ceil(2.4))
+
+# math.floor(x) => làm tròn xuống
+print(math.floor(2.7))
