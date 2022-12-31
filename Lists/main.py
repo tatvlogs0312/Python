08571aeb305154a75e4dd41b15cb140b6da4c70f
tatuan0312 <85239207+tatvlogs0312@users.noreply.github.com>
2022-12-31 21:18:04 +0700

    //List in python

--- Lists/main.py
+++ Lists/main.py
@@ -30,12 +30,51 @@ lists.append(2)
 print(lists)
 lists.append(3)
 lists.append(4)
+lists.append(5)
+lists.append(6)
+lists.append(7)
 print(f"List sau khi thêm phần tử : {lists}")
 
-# 2: remove(x) => xóa x ra khỏi list
-lists.remove(2)
-print(f"List sau khi xóa phần tử : {lists}")
+# 2: insert(x,y) => thêm giá trị y vào vị trí x
+lists.insert(0, 0)
+print(f"List sau khi thêm phần tử 0 : {lists}")
 
-# 3: list[x] => lấy giá trị ở index x
+# 3: remove(x) => xóa x ra khỏi list
+lists.remove(2)
+print(f"List sau khi xóa phần tử 2 : {lists}")
+
+# 4: list[x] => lấy giá trị ở index x
 print(f"Giá trị của list ở index 0 : {lists[0]}")
 print(f"Giá trị của list ở index 2 : {lists[2]}")
+
+# 5: list.index(x) => lấy giá trị ở vị trí x
+print(f"Giá trị của list ở index 1 : {lists.index(1)}")
+
+# 6: lists.count(x) => đếm số lượng x có trong list
+print(f"Số lượng phần tử 2 có trong list : {lists.count(2)}")
+print(f"Số lượng phần tử 5 có trong list : {lists.count(5)}")
+
+# 7: list.pop() => xóa phần tử cuối cùng, list.pop(x) => xóa phần tử ở vị trí x
+lists.pop()
+print(f"List sau khi xóa phần tử cuối cùng : {lists}")
+
+lists.pop(3)
+print(f"List sau khi xóa phần tử ở vị trí 3 : {lists}")
+
+# 8: list.clear() => xóa tất cả giá trị của list
+lists.clear()
+print(f"List sau khi clear : {lists}")
+
+lists2 = [1, 4, 2, 3, 7, 5, 6]
+# 9: list.sort() => sắp xếp list tăng dần
+lists2.sort()
+print(f"List sau khi sắp xếp tăng dần : {lists2}")
+
+# 10: list.reverse() => sắp xếp list giảm dần
+lists2.reverse()
+print(f"List sau khi sắp xếp giảm dần : {lists2}")
+
+# 11: copy() => copy toàn bộ phần tử từ list này sang list khác
+lists3 = lists2.copy()
+lists3.append(8)
+print(f"List sau khi được copy : {lists3}")
